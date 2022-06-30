10c7a3362fb7569811355f289a8d3849935e3c6e
Fernando Figueiras <fjafig@gmail.com>
2022-06-30 19:21:56 +0100

    add core library

---
+++ CoolGuessUpDown.Api/Controllers/GameStartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoolGuessUpDown.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameStartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
